6fdfa6363fd6b986d42320cb6551b42199f9629f
UnniBerget <96954530+UnniBerget@users.noreply.github.com>
2022-04-10 19:51:24 +0200

    Unni sin

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import Weather from './Weather';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1>Hello Unni Berget </h1>
+      < Weather />
+        <p>
+          Lærer react
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
