fdaf32de54df0caf6d332862d3c28b7f4c76bd5f
UnniBerget <96954530+UnniBerget@users.noreply.github.com>
2022-04-15 14:56:19 +0200

    Building 2

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,5 +1,27 @@
 import React from "react";
+import WeatherIcon from "WeatherIcon";
 
 export default function WeatherForecast () {
-    return "Hello from forecast"
+    return <div className="WeatherForecast">
+        <div className="row">
+            <div className="col">
+                <div className="WeatherForecast-day">Thur</div>
+                
+                <WeatherIcon code="01d" />
+                <div className="WeatherForecast-temperatures">
+                    <span className="WeatherForecast-temperatures-max">
+                    19
+                    </span>
+                    <span className="WeatherForecast-temperatures-min">
+                    10
+                    </span>
+                
+                        </div>
+
+            </div>
+
+        </div>
+
+
+    </div>
 }
